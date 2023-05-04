ccb207139cfb302dc29b4ae37706302b22851907
WordlessEcho <wordless@echo.moe>
2023-05-04 17:39:26 +0800

    Rename UserWithoutPassword to UserForReturn

--- src/main/java/moe/echo/bramblingnote/auth/UserClient.java
+++ src/main/java/moe/echo/bramblingnote/auth/UserClient.java
@@ -1,8 +1,7 @@
 package moe.echo.bramblingnote.auth;
 
-import moe.echo.bramblingnote.user.UserWithoutPassword;
+import moe.echo.bramblingnote.user.UserForReturn;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,5 +9,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("brambling-note-user")
 public interface UserClient {
     @GetMapping("/{email}")
-    UserWithoutPassword getByEmailAndPassword(@PathVariable String email, @RequestParam String password);
+    UserForReturn getByEmailAndPassword(@PathVariable String email, @RequestParam String password);
 }
