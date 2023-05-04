a11ae840b8f3c5d8fdedcd3d87613af025cf02f3
WordlessEcho <wordless@echo.moe>
2023-05-04 18:49:08 +0800

    Rename UserForReturn to UserForSession

--- src/main/java/moe/echo/bramblingnote/auth/Controller.java
+++ src/main/java/moe/echo/bramblingnote/auth/Controller.java
@@ -2,7 +2,7 @@ package moe.echo.bramblingnote.auth;
 
 import feign.FeignException;
 import jakarta.servlet.http.HttpSession;
-import moe.echo.bramblingnote.user.UserForReturn;
+import moe.echo.bramblingnote.user.UserForSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -20,9 +20,9 @@ public class Controller {
     }
 
     @GetMapping("/{email}")
-    public UserForReturn login(@PathVariable String email, @RequestParam String password, HttpSession session) {
+    public UserForSession login(@PathVariable String email, @RequestParam String password, HttpSession session) {
         try {
-            UserForReturn result = userClient.getByEmailAndPassword(email, password);
+            UserForSession result = userClient.getByEmailAndPassword(email, password);
             session.setAttribute("user", result);
             return result;
         } catch (FeignException e) {
--- src/main/java/moe/echo/bramblingnote/auth/UserClient.java
+++ src/main/java/moe/echo/bramblingnote/auth/UserClient.java
@@ -1,6 +1,6 @@
 package moe.echo.bramblingnote.auth;
 
-import moe.echo.bramblingnote.user.UserForReturn;
+import moe.echo.bramblingnote.user.UserForSession;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("brambling-note-user")
 public interface UserClient {
     @GetMapping("/{email}")
-    UserForReturn getByEmailAndPassword(@PathVariable String email, @RequestParam String password);
+    UserForSession getByEmailAndPassword(@PathVariable String email, @RequestParam String password);
 }
